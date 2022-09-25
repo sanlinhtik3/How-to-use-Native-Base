c54ee4fd20d96242b7173736cbd3d6b1bddbc485
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-09-25 13:05:31 +0630

    Width and Height

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
-import {Box, NativeBaseProvider, Text} from "native-base";
+import {Box, NativeBaseProvider} from "native-base";
 
 const App = () => {
   return (
       <NativeBaseProvider>
           {/* Typography */}
           <Box mt={10}>
-              <Text fontSize={'3xl'} fontWeight={'bold'}>Thank You</Text>
-              <Text lineHeight={'40'} letterSpacing={'5'}>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum dicta doloremque ex labore laboriosam nobis odit perspiciatis qui quibusdam, quisquam quo repellat repudiandae saepe, sed temporibus tenetur ullam ut.
-              </Text>
-              <Text fontSize={'2xl'} textAlign={'left'}>Hello</Text>
-              <Text fontSize={'2xl'} textTransform={'uppercase'}>Text Transform</Text>
-              <Text fontSize={'2xl'} underline>Text Transform</Text>
+              <Box bg={'pink.100'} w="20%" h="30px"></Box>
           </Box>
       </NativeBaseProvider>
   )
