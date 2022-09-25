c236b70dcb4168c9afdb126c57287dbee6a39aa3
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-09-25 14:33:33 +0630

    Shadow

--- App.js
+++ App.js
@@ -6,13 +6,15 @@ const App = () => {
           {/* Border */}
 
             {/*Parent*/}
-            <Box mt="10" mx={5} h={500} bg={'pink.100'}>
+            <Box mt="10" mx={5}>
 
                 {/*Child*/}
-                <Box position={'absolute'} zIndex={4} left={5} borderRadius={'xl'} w={20} h={20} bg={'pink.500'} borderWidth={5} borderColor={'pink.200'}/>
-                <Box position={'absolute'} zIndex={2} left={7} borderRadius={'xl'} w={20} h={20} bg={'green.500'} borderWidth={5} borderColor={'green.200'}/>
-                <Box position={'absolute'} zIndex={3} left={10} borderRadius={'xl'} w={20} h={20} bg={'rose.500'} borderWidth={5} borderColor={'rose.200'}/>
-                <Box position={'absolute'} left={12} borderRadius={'xl'} w={20} h={20} bg={'yellow.500'} borderWidth={5} borderColor={'yellow.200'}/>
+                <Box shadow="1" borderRadius={'xl'} w={20} h={20} bg={'pink.500'} borderWidth={5} borderColor={'pink.200'} mb="3"/>
+                <Box shadow="3" borderRadius={'xl'} w={20} h={20} bg={'pink.500'} borderWidth={5} borderColor={'pink.200'} mb="3"/>
+                <Box shadow="5" borderRadius={'xl'} w={20} h={20} bg={'pink.500'} borderWidth={5} borderColor={'pink.200'} mb="3"/>
+                <Box shadow="7" borderRadius={'xl'} w={20} h={20} bg={'pink.500'} borderWidth={5} borderColor={'pink.200'} mb="3"/>
+                <Box shadow="9" borderRadius={'xl'} w={20} h={20} bg={'pink.500'} borderWidth={5} borderColor={'pink.200'} mb="3"/>
+
             </Box>
 
       </NativeBaseProvider>
