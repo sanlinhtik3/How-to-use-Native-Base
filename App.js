7f0b432eac5dc5ee2bf323690b4004f01aefd2e9
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-09-25 13:47:27 +0630

    Flexbox

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import {Box, NativeBaseProvider} from "native-base";
 
+const ABox = () => {
+    return <Box p={5} bg={'pink.500'} m={5}></Box>
+}
+
 const App = () => {
   return (
       <NativeBaseProvider>
-          {/* Typography */}
-          <Box mt={10}>
-              <Box bg={'pink.100'} w="20%" h="30px"></Box>
-          </Box>
+          {/* flexbox */}
+          {/*<Box mt={10}>*/}
+
+          {/*    <Box display="flex" flexDirection={'row'} justifyContent={'space-evenly'} mb={10} bg="pink.100">*/}
+          {/*        <ABox/>*/}
+          {/*        <ABox/>*/}
+          {/*        <ABox/>*/}
+          {/*    </Box>*/}
+
+          {/*    <Box display="flex" flexDirection={'row'} h="400px" justifyContent={'center'} alignItems={"center"} bg="pink.100">*/}
+          {/*        <ABox/>*/}
+          {/*        <ABox/>*/}
+          {/*        <ABox/>*/}
+          {/*    </Box>*/}
+          {/*    */}
+          {/*</Box>*/}
+
+          <Box flex={1} bg={'pink.300'}></Box>
+
       </NativeBaseProvider>
   )
 }
