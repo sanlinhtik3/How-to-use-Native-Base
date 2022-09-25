e9989ec351a2341a72961d8960985a41678c7514
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-09-25 13:55:44 +0630

    Border

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
 import {Box, NativeBaseProvider} from "native-base";
 
-const ABox = () => {
-    return <Box p={5} bg={'pink.500'} m={5}></Box>
-}
-
 const App = () => {
   return (
       <NativeBaseProvider>
-          {/* flexbox */}
-          {/*<Box mt={10}>*/}
+          {/* Border */}
+            <Box mt="10" mx={5}>
 
-          {/*    <Box display="flex" flexDirection={'row'} justifyContent={'space-evenly'} mb={10} bg="pink.100">*/}
-          {/*        <ABox/>*/}
-          {/*        <ABox/>*/}
-          {/*        <ABox/>*/}
-          {/*    </Box>*/}
+                <Box w={20} h={20} bg={'pink.500'} borderWidth={5} borderColor={'pink.200'} mb={10}/>
+                <Box w={20} h={20} bg={'pink.500'} borderLeftWidth={5} borderColor={'pink.200'}/>
 
-          {/*    <Box display="flex" flexDirection={'row'} h="400px" justifyContent={'center'} alignItems={"center"} bg="pink.100">*/}
-          {/*        <ABox/>*/}
-          {/*        <ABox/>*/}
-          {/*        <ABox/>*/}
-          {/*    </Box>*/}
-          {/*    */}
-          {/*</Box>*/}
-
-          <Box flex={1} bg={'pink.300'}></Box>
+            </Box>
 
       </NativeBaseProvider>
   )
